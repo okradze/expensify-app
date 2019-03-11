7b2b645f32e5a359fd697f075547187e1c0de6a5
Mirian Okradze <okradzemirian@gmail.com>
2019-03-11 23:21:46 +0400

    Webpack production build

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/" activeClassName="is-active" exact={true}>Go home</NavLink>
-        <NavLink to="/create" activeClassName="is-active">Go to create page</NavLink>
-        <NavLink to="/edit" activeClassName="is-active">Go to edit page</NavLink>
-        <NavLink to="/help" activeClassName="is-active">Go to help page</NavLink>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     </header>
 );
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,47 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
-};
+module.exports = (env) => {
+    const isProducion = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' });
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },     {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+               }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProducion ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
+}
