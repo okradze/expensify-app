790af41c032716ed47ceaf5b74b5c04c5f8c5914
Mirian Okradze <okradzemirian@gmail.com>
2019-03-12 10:57:45 +0400

    Webpack replaced by Parcel

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routes/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
 import 'normalize.css/normalize.css';
 import './styles/main.scss';
 import 'react-dates/lib/css/_datepicker.css';
