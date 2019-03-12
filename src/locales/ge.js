36bdf19be78668f175ff784b06bff7188166d175
Mirian Okradze <okradzemirian@gmail.com>
2019-03-12 12:06:59 +0400

    Added locales

---
+++ src/locales/ge.js
@@ -0,0 +1,21 @@
+import numeral from 'numeral';
+
+numeral.register('locale', 'ge', {
+    delimiters: {
+        thousands: ' ',
+        decimal: ','
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal : function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '₾'
+    }
+});
+numeral.locale('ge');
