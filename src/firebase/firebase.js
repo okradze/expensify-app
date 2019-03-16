f3ac729d0c4637cb502398b0942e38ba827b81c9
Mirian Okradze <okradzemirian@gmail.com>
2019-03-16 11:33:15 +0400

    Setup test db env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBOsVPpyccgyWelkSnrieyNvik6GggoUi4",
-    authDomain: "expensify-593bf.firebaseapp.com",
-    databaseURL: "https://expensify-593bf.firebaseio.com",
-    projectId: "expensify-593bf",
-    storageBucket: "expensify-593bf.appspot.com",
-    messagingSenderId: "809735488178"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_OFF_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
@@ -14,38 +14,3 @@ firebase.initializeApp(config);
 const db = firebase.database();
 
 export { firebase, db as default };
-
-/* const getExpenses = (snapshot) => {
-    const expenses = [];
-    snapshot.forEach((child) => {
-        expenses.push({
-            id: child.key,
-            ...child.val()
-        });
-    });
-    console.log(expenses);
-};
-
-database.ref('expenses').on('value', getExpenses);
-database.ref('expenses').on('child_removed', getExpenses);
-database.ref('expenses').on('child_changed', getExpenses);
-database.ref('expenses').on('child_added', getExpenses);
-
-database.ref('expenses').push({
-    description: 'Rent',
-    note: '',
-    amount: 125000,
-    createdAt: 0
-});
-
-database.ref().once('value').then((snapshot) => {
-    const data = snapshot.val();
-    console.log(`${data.name} is ${data.job.title} at ${data.job.company}`);
-}).catch(err => {
-    console.log(err);
-});
-
-database.ref().on('value', (snapshot) => {
-    const data = snapshot.val();
-    console.log(`${data.name} is ${data.job.title} at ${data.job.company}`);
-});*/
--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,12 @@
-const package = require('./package.json');
+const webpack = require('webpack');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if(process.env.NODE_ENV === 'test'){
+    require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development' });
+}
 
 module.exports = {
     entry: {
@@ -30,5 +38,15 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+            'process.env.FIREBASE_OFF_DOMAIN': JSON.stringify(process.env.FIREBASE_OFF_DOMAIN),
+            'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+            'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+            'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+            'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+        })
+    ]
 };
