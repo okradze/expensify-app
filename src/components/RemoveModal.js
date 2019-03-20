318058d6e6a0e8a2074f7ccab02d60022a406ddb
Mirian Okradze <okradzemirian@gmail.com>
2019-03-20 16:34:12 +0400

    Fixed modal never mind button

--- src/components/RemoveModal.js
+++ src/components/RemoveModal.js
@@ -10,7 +10,7 @@ export const RemoveModal = ({ isModalOpen, changeModalState, onRemove }) => (
         className="modal" >
 
         <h2>Are you sure to remove an expense?</h2>
-        <button className="button button--blue modal__button">Never mind</button>
+        <button className="button button--blue modal__button" onClick={changeModalState}>Never mind</button>
         <button className="button button--red" onClick={onRemove}>Sure</button>
     </Modal>
 );
