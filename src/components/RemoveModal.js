83d8ecd7ac2f49f6936b31119cad5ec2fc195a64
Mirian Okradze <okradzemirian@gmail.com>
2019-03-20 22:44:16 +0400

    Fixed never mind button

--- src/components/RemoveModal.js
+++ src/components/RemoveModal.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-export const RemoveModal = ({ isModalOpen, changeModalState, onRemove }) => (
+export const RemoveModal = ({ isModalOpen, closeModal, onRemove }) => (
     <Modal
         isOpen={isModalOpen}
-        onRequestClose={changeModalState}
+        onRequestClose={closeModal}
         contentLabel="Remove expense"
         appElement={document.getElementById('app')}
         className="modal" >
 
         <h2>Are you sure to remove an expense?</h2>
-        <button className="button button--blue modal__button" onClick={changeModalState}>Never mind</button>
+        <button className="button button--blue modal__button" onClick={closeModal}>Never mind</button>
         <button className="button button--red" onClick={onRemove}>Sure</button>
     </Modal>
 );
